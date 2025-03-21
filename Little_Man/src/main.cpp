330164dabc29f3ed0495fafade1d2fbd17cb9fa3
Kami6618 <kyuce790@gmail.com>
2025-03-21 20:39:25 +0100

    Update main.cpp

--- Little_Man/src/main.cpp
+++ Little_Man/src/main.cpp
@@ -13,100 +13,108 @@ int dinopos = 4;
 bool gameOver = false;
 unsigned long jumptime = 0;
 const unsigned long jumpdauer = 500;
+bool buttonPressed = false;
+int gapCounter = random(3, 7); // Zufälliger Abstand zwischen Hindernissen
 
 void checkCollision() {
-
-  if (terrain[dinopos] == 'X' && dinopos == 4) {
+  if (terrain[4] == 'X' && dinopos == 4) {
     gameOver = true;
   }
-
 }
 
 void init_terrain() {
   for (int i = 0; i < DISPLAY_LENGTH; ++i) {
-    int randomValue = random(2); // 0 oder 1
-    if (randomValue == 0)
-      terrain[i] = ' '; // Leeres Terrain
-    else
-      terrain[i] = 'X'; // Volles Terrain
+    terrain[i] = ' '; // Start mit leerem Terrain
   }
 }
 
 void displayTerrain() {
-  lcd.setCursor(0, 1); // Setze den Cursor auf die zweite Zeile
+  lcd.setCursor(0, 1);
   for (int i = 0; i < DISPLAY_LENGTH; ++i) {
-    lcd.print(terrain[i]); // Zeige das Terrain an
+    lcd.print(terrain[i]);
   }
 }
 
 void displayDino() {
-  
-if (dinopos == 3) {
-    lcd.setCursor(4, 0); // Setze den Cursor auf die erste Zeile, Position 4
-    lcd.print("D"); // Zeige den Dino in der Luft an
-  } else {
-    lcd.setCursor(4, 1); // Setze den Cursor auf die zweite Zeile, Position 4
-    lcd.print("D"); // Zeige den Dino am Boden an
+  lcd.setCursor(4, dinopos == 3 ? 0 : 1);
+  lcd.print("D");
+}
 
+void jump() {
+  if (dinopos == 4) {  
+    dinopos = 3;
+    jumptime = millis();
   }
 }
 
-
-void jump() {
-  if (dinopos == 4) { // Wenn der Dino auf der Grundposition ist
-    dinopos = 3;
-    jumptime = millis(); 
-  }
+void updateJump() {
   if (dinopos == 3 && millis() - jumptime > jumpdauer) {
-    if (terrain[4] != 'X') {
-
-      dinopos = 4; // Zurück auf den Boden
-
+    if (terrain[4] == ' ') {  
+      dinopos = 4;  
     }
-  } 
+  }
 }
 
 void updateTerrain() {
-  // Terrain nach links verschieben
   for (int i = 0; i < DISPLAY_LENGTH - 1; ++i) {
-    terrain[i] = terrain[i + 1]; // Verschiebe jedes Element nach links
+    terrain[i] = terrain[i + 1];
   }
-  // Neues Terrain am Ende hinzufügen
-  int randomValue = random(2); // 0 oder 1
-  if (randomValue == 0)
-    terrain[DISPLAY_LENGTH - 1] = ' '; // Leeres Terrain
-  else
-    terrain[DISPLAY_LENGTH - 1] = 'X'; // Volles Terrain
+
+  if (gapCounter > 0) {  
+    terrain[DISPLAY_LENGTH - 1] = ' ';  
+    gapCounter--;
+  } else {
+    terrain[DISPLAY_LENGTH - 1] = 'X';  
+    gapCounter = random(3, 7);  
+  }
+}
+
+void resetGame() {
+  gameOver = false;
+  dinopos = 4;
+  gapCounter = random(3, 7);
+  init_terrain();
+  lcd.clear();
 }
 
 void setup() {
   pinMode(TASTER, INPUT_PULLUP);
   lcd.init();
   lcd.backlight();
-  init_terrain(); 
+  init_terrain();
   displayTerrain();
   displayDino();
 }
 
 void loop() {
-
   if (!gameOver) {
     int buttonState = digitalRead(TASTER);
-    if (buttonState == LOW) {
-      jump();
-    }
-    updateTerrain(); // Terrain aktualisieren
-    lcd.clear(); // LCD löschen
-    displayTerrain(); // Terrain anzeigen
-    displayDino(); // Dino anzeigen
-    checkCollision(); // Überprüfen auf Kollision
-    delay(500); // Wartezeit für die Animation
 
+    if (buttonState == LOW && !buttonPressed) {  
+      jump();  
+      buttonPressed = true;
+    }
+
+    if (buttonState == HIGH) {  
+      buttonPressed = false;
+    }
+
+    updateJump();  
+    updateTerrain();
+    lcd.clear();
+    displayTerrain();
+    displayDino();
+    checkCollision();
+    delay(500);
   } else {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Game Over!");
     lcd.setCursor(0, 1);
-    lcd.print("Press Reset");
+    lcd.print("Press Button");
+
+    while (digitalRead(TASTER) == HIGH); // Warten auf Tastendruck
+    delay(300); // Entprellen
+    resetGame();
   }
 }
